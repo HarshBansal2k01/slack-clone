d9f4d0804b611590942a5b5f6b85d05831a18629
HarshBansal2k01 <ihbbro14@gmail.com>
2022-01-26 17:05:40 +0530

    yay

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
-import styled from 'styled-components';
-import Sidebar from './Components/Sidebar';
+import styled from "styled-components";
+import Sidebar from "./Components/Sidebar";
+import Chat from "./Components/Chat";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <>
-        <Header/>
-        <AppBody>
-          <Sidebar/>
-          <Routes>
-          <Route path="/" exact>
-            {/* chat */}
-          </Route>
-        </Routes>
-        </AppBody>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Chat/>}>
+              </Route>
+            </Routes>
+          </AppBody>
         </>
-    </Router>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
 
 const AppBody = styled.div`
   display: flex;
--- src/Components/SidebarOption.jsx
+++ src/Components/SidebarOption.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { enterRoom } from "../features/appSlice";
 import { db } from "../firebase";
 
-function SidebarOption({ Icon, title,addChannelOption, id}) {
 
 
-    const addChannel = () =>{
-        const channelName = prompt("Please enter the channel name");
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+    const dispatch = useDispatch();
 
-        if(channelName){
-            db.collection('rooms').add({
-                Name: channelName,
-            })
-        }
-    };
-    const selectChannel = () =>{};
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+
+    if (channelName) {
+      db.collection("rooms").add({
+        Name: channelName,
+      });
+    }
+  };
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
   return (
     <SidebarOptionContainer
-    onClick={addChannelOption ? addChannel : selectChannel}>
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
         <h3>{title}</h3>
@@ -34,25 +47,25 @@ function SidebarOption({ Icon, title,addChannelOption, id}) {
 export default SidebarOption;
 
 const SidebarOptionContainer = styled.div`
-    display: flex;
-    font-size: 12px;
-    align-items: center;
-    padding-left: 2px;
-    cursor: pointer;
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
 
-    :hover{
-        opacity: 0.9;
-        background-color: #340e36;
-    } 
-    >h3{
-        font-weight: 500;
-    }
-    >h3>span{
-        padding: 15px;
-    }
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+  > h3 {
+    font-weight: 500;
+  }
+  > h3 > span {
+    padding: 15px;
+  }
 `;
 
-const SidebarOptionChannel = styled.div`
-
-
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
 `;
