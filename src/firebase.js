5246aacff6612971d12be66b22e1d9bf7a6d3e41
HarshBansal2k01 <ihbbro14@gmail.com>
2022-01-26 12:25:28 +0530

    sidebar option

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
 const firebaseConfig = {
   apiKey: "AIzaSyAsFjvMU23UKAb2ohRKg_Z8EyPXUz6GwpQ",
   authDomain: "slack-clone-7ac71.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "637831382311",
   appId: "1:637831382311:web:af2d4bba9192121143d502"
 };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {db, auth,provider};
